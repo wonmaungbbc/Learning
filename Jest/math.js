4f09ab485d91b71891763be24705f3a5eecf6dc0
Won Maung Thein <wonmaung.thein@bbc.co.uk>
2019-02-21 16:55:14 +0000

    added a sayHi function

--- Jest/math.js
+++ Jest/math.js
@@ -2,5 +2,6 @@ const sum = (a, b) => a + b;
 const mul = (a, b) => a * b;
 const sub = (a, b) => a - b;
 const div = (a, b) => a / b;
+const sayHi = (message, name) => `${message} ${name}`;
 
-module.exports = { sum, mul, sub, div };
+module.exports = { sum, mul, sub, div, sayHi };
--- Jest/math.test.js
+++ Jest/math.test.js
@@ -1,4 +1,4 @@
-const { sum, mul, sub, div } = require("./math");
+const { sum, mul, sub, div, sayHi } = require("./math");
 
 test("Adding 1 + 1 equals 2", () => {
   expect(sum(1, 1)).toBe(2);
@@ -11,4 +11,8 @@ test("Subtracting 1 - 1 equals 0", () => {
 });
 test("Dividing 1 / 1 equals 1", () => {
   expect(div(1, 1)).toBe(1);
+});
+
+test("Say Hello Won", () => {
+  expect(sayHi("Hello", "Won")).toBe("Hello Won");
 });
